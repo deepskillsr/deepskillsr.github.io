1a8dc9a6cd8fdc5345011591ff762af089bc82fc
Andreas Novotny <andreas.novotny@su.se>
2018-11-13 20:41:45 +0100

    Adding Andreas files

---
+++ AndreasFiles/template_presentation_RaukR/packages.R
@@ -0,0 +1,24 @@
+---
+  title: "Functions and Packages in R"
+subtitle: "Developing skills in R, DEEP, SU"
+author: "Andreas Novotny"
+date: "14 November 2018"
+institute: NBIS, SciLifeLab
+keywords: r, RaukR, markdown
+output: 
+  xaringan::moon_reader:
+  
+  encoding: 'UTF-8'
+self_contained: false
+chakra: 'remark-latest.min.js'
+css: 'presentation_noLogo.css'
+lib_dir: libs
+nature:
+  ratio: '4:3'
+highlightLanguage: r
+highlightStyle: github
+highlightLines: true
+countIncrementalSlides: false
+slideNumberFormat: "deepRskills 2018 . %current%/%total%"
+
+---
